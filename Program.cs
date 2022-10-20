751aef05db0a89f331fcc97a66c8ceca20055879
nickil312 <112865509+nickil312@users.noreply.github.com>
2022-10-20 17:00:07 +0300

    part2

--- Program.cs
+++ Program.cs
@@ -11,28 +11,66 @@ namespace Practicheskai_4
     {
         public static int position = 1;
         public static int xposition = 0;
-        // переменные для считывания количества сдвигов по x и y
+        public static DateTime selectedDate = DateTime.Now;
+        
         static void Main()
         {
-            
-            //Soul gg = new Soul();
-            //gg.get2();
-            //Soul.get1();
-            
-            Console.SetCursorPosition(2, 0);
-            Console.WriteLine("Выбрана дата");
-            Console.SetCursorPosition(0, 1);
-            Console.WriteLine("  1: Прийти на пары");
-            Console.SetCursorPosition(0, 2);
-            Console.WriteLine("  2: Сделать практические");
-            Console.WriteLine("  3: Купить продукты");
+            Soul gg1 = new Soul()
+            {
+                name = "НЕТ ПАР",
+                description = "Сиди дома",
+                SoulDay = new DateTime(2022, 10, 19),
+            };
+            Soul gg2 = new Soul()
+            {
+                name = "Прийти на пары на нежку",
+                description = "С 1 по 4 пару",
+                SoulDay = new DateTime(2022, 10, 20),
+            };
+            Soul gg3 = new Soul()
+            {
+                name = "Прийти на пары на нахим",
+                description = "С 3 по 5 пару",
+                SoulDay = new DateTime(2022, 10, 21),
+            };
+            Soul gg4 = new Soul()
+            {
+                name = "Прийти на пары в дистанте",
+                description = "С 2 по 4 пару",
+                SoulDay = new DateTime(2022, 10, 12),
+            };
+            Soul gg5 = new Soul()
+            {
+                name = "Сделать практические (оч много)",
+                description = "Сделать 1 и 2",
+                SoulDay = new DateTime(2022, 10, 14),
+            };
+            Soul gg6 = new Soul()
+            {
+                name = "Сделать практические по питону",
+                description = "Доделать 1 и сделать 2",
+                SoulDay = new DateTime(2022, 10, 14),
+            };
+            Soul gg7 = new Soul()
+            {
+                name = "gg",
+                description = "Сделать 1 и 2",
+                SoulDay = new DateTime(2022, 10, 14),
+            };
+            Soul.souls.Add(gg1);
+            Soul.souls.Add(gg2);
+            Soul.souls.Add(gg3);
+            Soul.souls.Add(gg4);
+            Soul.souls.Add(gg5);
+            Soul.souls.Add(gg6);
+            Soul.souls.Add(gg7);
             // нач текст без стрелки 
 
             ConsoleKeyInfo key = Console.ReadKey();
             //считывается кнопка (стрелка вниз/вверх)
             ConsoleKeyInfo a = Console.ReadKey();
             //считывается кнопка (esc для заканчивания программы)
-
+          
             while (a.Key != ConsoleKey.Escape)
             {
 
@@ -52,24 +90,55 @@ namespace Practicheskai_4
                 }
                 if (position > 6)
                 {
-                    position = 6;
+                    position = 1;
                 }
-                // цикл для сдвига стрелочки
                 Console.Clear();
                 Console.SetCursorPosition(2, 0);
                 Console.WriteLine("Выбрана дата");
-                Console.SetCursorPosition(0, 1);
                 Console.WriteLine("  1: Прийти на пары");
-                Console.SetCursorPosition(0, 2);
                 Console.WriteLine("  2: Сделать практические");
-                Console.SetCursorPosition(0, 3);
                 Console.WriteLine("  3: Купить продукты");
                 Console.SetCursorPosition(0, position);
                 Console.WriteLine($"{position}>");
                 key = Console.ReadKey();
-                //Считывание стрелочки и ее предвижение по нему
-                Soul.hey(key);
-                //вызов класса
+                ConsoleKeyInfo upanddown = Console.ReadKey();
+                if (position == 1 && upanddown.Key == ConsoleKey.Enter)
+                {
+                    Console.Clear();
+                    
+                    foreach (Soul item in Soul.souls)
+                    {
+                        Console.WriteLine("Нажмите вправо для просмотра 1 записи ");
+                        
+                        ConsoleKeyInfo rightbutton = Console.ReadKey();
+                        if (rightbutton.Key == ConsoleKey.RightArrow)
+                        {
+                            selectedDate = selectedDate.AddDays(+1);
+                            if (item.SoulDay == selectedDate)
+                            {
+                            Console.WriteLine("  " + item.name);
+
+                            }
+                            
+
+                        }
+                        
+                        else if (rightbutton.Key == ConsoleKey.LeftArrow)
+                        {
+                            
+                            selectedDate = selectedDate.AddDays(-1);
+                            if (item.SoulDay == selectedDate)
+                            {
+                                Console.WriteLine("  " + item.name);
+
+                            }
+                        }
+
+
+                    }
+                }
+                
+                
             }
 
 
--- Soul.cs
+++ Soul.cs
@@ -4,102 +4,12 @@ namespace Practicheskai_4
     public class Soul
     {
 
+        public static List<Soul> souls = new List<Soul>();
+        public string name;
+        public string description;
+        public DateTime SoulDay;
 
-        //int gg1 = 1;
-        static DateTime dateTime1 = new DateTime(2022, 10, 8);
-        static DateTime dateTime2 = new DateTime(2022, 9, 4);
-        static DateTime dateTime3 = new DateTime(2022, 10, 5);
-        // даты которые будут выписываться
-        static List<string> text = new List<string>() { "Прийти на пары на нахим", "-----------------------", "Описание: С 1 по 4 пару" };
-        static List<string> text2 = new List<string>() {  "Прийти на пары на нежку", "----------------------", "Описание: С 2 по 3 пару "};
 
-        
-        //    public static void get1()
-        //    {
-        //        Console.WriteLine("ghbd");
-        //    }
-        //    public void get2()
-        //    {
-        //        int a = 1;
-        //        int b = 3;
-        //        int c = b + a;
-        //        Console.WriteLine(c);
-        //    }
-        public static void hey(ConsoleKeyInfo presskey)
-
-        {
-            if (Programm.position == 1 && presskey.Key == ConsoleKey.Enter)
-            {
-                
-                
-                if (Programm.xposition == 0)
-                {
-                    Console.Clear();
-                    foreach (string text1 in text)
-                    {
-                        Console.WriteLine(text1);
-                    }
-                    Console.WriteLine(dateTime1);
-                    ConsoleKeyInfo presskey2 = Console.ReadKey();
-                    if (presskey2.Key == ConsoleKey.RightArrow)
-                    {
-                        Programm.xposition++;
-                        hey(presskey2);
-                    }
-                    
-                }
-                
-
-                if (Programm.xposition == 1)
-                {
-                    Console.Clear();
-                    foreach (string text0 in text2)
-                    {
-                        Console.WriteLine(text0);
-                    }
-                    Console.WriteLine(dateTime2);
-                    ConsoleKeyInfo presskey3 = Console.ReadKey();
-                    if (presskey3.Key == ConsoleKey.RightArrow)
-                    {
-                        Programm.xposition++;
-                        hey(presskey3);
-                    }
-
-                    else if (presskey3.Key == ConsoleKey.LeftArrow)
-                    {
-                        Programm.xposition--;
-                        hey(presskey3);
-                    }
-                }
-                
-
-                if (Programm.xposition == 2)
-                {
-                    Console.Clear();
-                    Console.WriteLine("Прийти на пары на дистанте");
-                    Console.WriteLine("-----------------------");
-                    Console.WriteLine("Описание: С 4 по 5 пару");
-                    Console.WriteLine(dateTime3);
-                }
-                ConsoleKeyInfo presskey4 = Console.ReadKey();
-                if (presskey4.Key == ConsoleKey.LeftArrow)
-                {
-                    Programm.xposition--;
-                    hey(presskey4);
-                }
-            }
-            
-            if (Programm.position == 2 && presskey.Key == ConsoleKey.Enter)
-            {
-                Console.WriteLine("gg wp");
-
-            }
-
-            if (Programm.position == 3)
-            {
-                
-            }
-        }
         
     }
 }
