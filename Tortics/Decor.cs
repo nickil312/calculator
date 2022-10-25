e046786920a2bdd9c1258f05e0b4ffb92a187979
nickil312 <112865509+nickil312@users.noreply.github.com>
2022-10-25 12:30:56 +0300

    архитектура классов

---
+++ Tortics/Decor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Tortics
+{
+    public class Decor
+    {
+        public Decor(int amount = 0, string view = "", string color = "", int size = 1)
+        {
+            int Amount = amount;
+            string View = view;
+            string Color = color;
+            int Size = size;
+        }
+        int Amount = 0;
+        string View = "";
+        string Color = "";
+        int Size = 1;
+
+    }
+}
+
---
+++ Tortics/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Tortics
+{
+    public class Order
+    {
+        //дефолт конструктор, пока не трогать
+        public Order()
+        {
+
+        }
+        public string Form = "";
+        public int Size = 0;
+        public string Taste = "";
+        public int Amount = 1;
+        public string Glaze = "";
+        public Decor Decor;
+     
+    }
+}
+
