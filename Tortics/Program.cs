9020fdd0ec1d454d9de543e2c9937b3e745e6187
nickil312 <112865509+nickil312@users.noreply.github.com>
2022-10-25 15:27:22 +0300

    красивые методы

--- Tortics/Program.cs
+++ Tortics/Program.cs
@@ -8,12 +8,34 @@ namespace Tortics
 {
     internal class Program
     {
+        /// <summary>
+        /// Более комфортный путь создания через конструктор
+        /// </summary>
+        /// <param name="amount">Количество деталек декора</param>
+        /// <param name="view">Вид деталей</param>
+        /// <param name="color">Цвет деталей</param>
+        /// <param name="size">Введите размер деталек декора</param>
+        /// <returns>Объект сразу с декором</returns>
 
-         
+        static Order OrderDecor(int amount = 0, string view = "", string color = "", int size = 0)
+        {
+            Console.WriteLine("Введите количество деталек декора: ");
+            int.TryParse(Console.ReadLine(), out amount);
+            Console.WriteLine("Введите размер деталек декора: ");
+            int.TryParse(Console.ReadLine(), out size);
+            Console.WriteLine("В виде чего будут детальки?");
+            view = Console.ReadLine();
+            Console.WriteLine("В какого цвета будут детальки?");
+            color = Console.ReadLine();
+            return new Order(new Decor(), amount, view, color, size);
+        }
+        
         static void Main()
         {
-            var a = new Order(new Decor(), int.Parse(Console.ReadLine()), "Цветочки", "Красный", 3);
-            Console.WriteLine($"Размер торта {a.Amount}\n Количество декора {a.Decor.Amount}");
+            var a = OrderDecor(12, size: 13);
+            Console.WriteLine($"Размер торта {a.Amount}" +
+                $"\n Количество декора {a.Decor.Amount}" +
+                $"\n {a.Decor.Color}");
             Console.ReadKey();
         }
     }
