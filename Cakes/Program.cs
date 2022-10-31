0395f8c23b776ac9ac07790db31a78db9f377e46
nickil312 <112865509+nickil312@users.noreply.github.com>
2022-10-31 17:04:54 +0300

    end of program

--- Cakes/Program.cs
+++ Cakes/Program.cs
@@ -14,18 +14,22 @@ namespace Cakes
         public static int position = 1;
         public static int secondposition = 1;
         public static int thirdposition = 1;
+        public static int fourthposition = 1;
+        public static int sixthposition = 1;
+        public static int seventhposition = 1;
+        public static int eightposition = 1;
         static void Main()
         {
-            
+            Console.CursorVisible = false;
             int n;
             Console.WriteLine("Введите количество тортов,которые хотите купить");
             n = int.Parse(Console.ReadLine());
             for (int i = 0; i < n; i++)
             {
-                CakeDesign.Menu(position);
+                CakeDesign.Menu(position,thirdposition,fourthposition, sixthposition, seventhposition, eightposition);
                 var OrderObject = CakeDesign.CakeList[CakeDesign.CakeList.Count - 1];
                 string ourOrder = $"Заказ от {DateTime.Now}\n" +
-                    $"\tЗаказ: {OrderObject.shape},{OrderObject.size}\n" +
+                    $"\tЗаказ: {OrderObject.shape},{OrderObject.size},{OrderObject.taste},{OrderObject.amountoflevels},{OrderObject.glaze},{OrderObject.decor}\n" +
                     $"\tЦена: {OrderObject.price}\n";
                     
                 File.AppendAllText("/Users/Nickill/Desktop/gg.txt", ourOrder);
@@ -34,7 +38,7 @@ namespace Cakes
                 Console.Clear();
                 
                 Console.WriteLine($"Сумма заказа {CakeDesign.Ck_price}");
-                Console.WriteLine("Хотите зааказть еще один тортик?\n" +
+                Console.WriteLine("Хотите заказать еще один тортик?\n" +
                     "1:ДА\n" +
                     "2:НЕТ\n");
                 
@@ -46,6 +50,7 @@ namespace Cakes
                 }
                 else
                 {
+                    Thread.Sleep(3000);
                     Console.WriteLine("Торт(ы) заказан(ы)\n" +
                         "Проверьте чек");
                 }
